ce91cbce0a31c2777ccb18f3ff34d10191f0360d
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2025-03-27 15:04:36 +0100

    feat: add postgres database

--- src/pkmd/settings.py
+++ src/pkmd/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'pkmd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': env.str('POSTGRES_PORT', default='5432'),
     },
 }
 
